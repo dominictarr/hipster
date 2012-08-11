caf4844e760d2726fd093a64a573f9bf801f3b48
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-11 18:13:41 +0200

    delete selected text with delete/backspace

--- plugins/entry.js
+++ plugins/entry.js
@@ -10,8 +10,14 @@ module.exports = function (doc, keys, cursor) {
     if(key.shift && key.name.length === 1)
       key.name = key.name.toUpperCase()
 
-    if     (key.name == 'delete')    doc.delete(1)
-    else if(key.name == 'backspace') doc.delete(-1)
+    if     (key.name == 'delete') {
+      if(doc.marks) doc.clearMarked()
+      else          doc.delete(1)
+    }
+    else if(key.name == 'backspace') {
+      if(doc.marks) doc.clearMarked()
+      else          doc.delete(-1)
+    }
     else if(key.name == 'enter')     doc.newline()
     else if(key.name == 'tab')       doc.write('  ') 
     else if(' ' <= ch && ch <= '~')  doc.write(ch)
