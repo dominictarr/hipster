32106c1a0347ecc3777287a1930c3975c742e166
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-11 19:50:07 +0200

    Ctrl-del, Ctrl-backspace to delete whole words

--- plugins/entry.js
+++ plugins/entry.js
@@ -7,15 +7,19 @@
 module.exports = function (doc, keys, cursor) {
   keys.on('keypress', function (ch, key) {
 
+    console.error(key)
+
     if(key.shift && key.name.length === 1)
       key.name = key.name.toUpperCase()
 
     if     (key.name == 'delete') {
       if(doc.marks) doc.clearMarked()
+      else if(key.ctrl) doc.mark().next().mark().clearMarked()
       else          doc.delete(1)
     }
     else if(key.name == 'backspace') {
       if(doc.marks) doc.clearMarked()
+      else if(key.ctrl) doc.mark().prev().mark().clearMarked()
       else          doc.delete(-1)
     }
     else if(key.name == 'enter')     doc.newline()
