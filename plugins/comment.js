37a93c26e472d2692edb76dd078ce6ca08dcfdef
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-13 01:57:00 +0200

    comment the current line, if no selection

--- plugins/comment.js
+++ plugins/comment.js
@@ -8,6 +8,7 @@
 
 // I spend most of my time editing js, though. 
 // so I'll leave that for when it's more important.
+// don't comment out empty lines?
 
 function comment (line) {
   return '//' + line
@@ -17,8 +18,7 @@ function uncomment (line) {
 }
 
 function isCommented (line) {
-  var r =  /^\s*\/\//.test(line)
-   console.error('COMMENTED?', r, line)
+  var r =  /^\s*\/\//.test(line) || /^\s*$/.test(line)
   return r
 }
 
@@ -28,20 +28,24 @@ module.exports = function (doc, keys) {
 
   keys.on('keypress', function (ch, key) {
 
-    if('k' == key.name && key.ctrl && doc.marks) {
+    if('k' == key.name && key.ctrl) {
+      if(!doc.marks)
+        doc.start().mark().down().mark().up().move()
       var m = doc.marks[0]
       var M = doc.marks[1]
-
+      var from = m.y
+      //only indent do not comment the last line if the cursor is at 0
+      var to   = M.x || M.y == m.y ? M.y : M.y - 1
       //first decide if we will comment or uncomment.
       //uncomment, if the lines all have comments at the start.
       //else, comment.
 
       var commented = true
-      for(var i = m.y; i <= M.y; i++)
+      for(var i = from; i <= to; i++)
         if(!isCommented(doc.lines[i]))
           commented = false
 
-      for(var i = m.y; i <= M.y; i++)
+      for(var i = from; i <= to; i++)
         doc.updateLine(i,
           (commented ? uncomment : comment) (doc.lines[i])
         )
