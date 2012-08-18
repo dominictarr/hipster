0b81315bc5f2d118631155790fdad488a77596ae
Dominic Tarr <dominic.tarr@gmail.com>
2012-08-18 11:46:53 +0200

    refactor preferred column & add to plugins/entry

--- plugins/entry.js
+++ plugins/entry.js
@@ -7,8 +7,6 @@
 module.exports = function (doc, keys, cursor) {
   keys.on('keypress', function (ch, key) {
 
-    console.error(key)
-
     if(key.shift && key.name.length === 1)
       key.name = key.name.toUpperCase()
 
@@ -16,15 +14,18 @@ module.exports = function (doc, keys, cursor) {
       if(doc.marks) doc.clearMarked()
       else if(key.ctrl) doc.mark().next().mark().clearMarked()
       else          doc.delete(1)
+      doc.pref()
     }
     else if(key.name == 'backspace') {
       if(doc.marks) doc.clearMarked()
       //quirk in keypress... TODO should make fix.
       else if(key.ctrl || key.sequence == '\b') doc.mark().prev().mark().clearMarked()
       else          doc.delete(-1)
+
+      doc.pref()
     }
-    else if(key.name == 'tab')       doc.write('  ') 
-    else if(key.name == 'enter')     doc.clearMarked().newline()
-    else if(' ' <= ch && ch <= '~')  doc.clearMarked().write(ch)
+    else if(key.name == 'tab')       doc.write('  ') .pref()
+    else if(key.name == 'enter')     doc.clearMarked().newline().pref()
+    else if(' ' <= ch && ch <= '~')  doc.clearMarked().write(ch).pref()
   })
 }
--- plugins/movement.js
+++ plugins/movement.js
@@ -1,72 +1,71 @@
 
 module.exports = function (doc, keys, cursor) {
   var rc = this.config
-  var preferred = 0
-
-  function pref () {
-    preferred = doc.column
-    console.error('PREFERRED COLUMN', preferred)
-  }
-  function toPref() {
-    if('undefined' === typeof preferred) doc.move()
-    if(doc.line().length < preferred)
-      doc.column = doc.line().length - 1
-    else
-      doc.column = preferred
-    doc.move()
-  }
+//  var preferred = 0
+//
+//  function pref () {
+//    preferred = doc.column
+//    console.error('PREFERRED COLUMN', preferred)
+//  }
+//  function toPref() {
+//    if('undefined' === typeof preferred) doc.move()
+//    if(doc.line().length < preferred)
+//      doc.column = doc.line().length - 1
+//    else
+//      doc.column = preferred
+//    doc.move()
+//  }
 
   keys.on('keypress', function (ch, key) {
 
       if(!key.ctrl) {
 
-        if(key.name == 'up'   ) {
-          (doc.isFirstLine() ? doc.start() : doc.up())
-          toPref()
-        }
+        if(key.name == 'up'   )
+          (doc.isFirstLine() ? doc.start() : doc.up()).toPref().move()
+       
         if(key.name == 'down' ) {
           (doc.isLastLine() ? doc.end() : doc.down())
-          toPref()
+          .toPref().move()
         }
         if(key.name == 'left' ) {
-          ((doc.isFirst() && !doc.isFirstLine() ? doc.up().end() : doc.left())).move()
-          pref()
+          ((doc.isFirst() && !doc.isFirstLine() ? doc.up().end() : doc.left())).pref().move()
         }
         if(key.name == 'right') {
-          ((doc.isLast() && !doc.isLastLine() ? doc.down().start() : doc.right())).move()
-          pref()
+          ((doc.isLast() && !doc.isLastLine() ? doc.down().start() : doc.right())).pref().move()
         }
-        if(key.name == 'end') doc.end().move(), pref()
-        if(key.name == 'home') doc.start().move(), pref()
+        if(key.name == 'end') doc.end().pref().move()
+        if(key.name == 'home') doc.start().pref().move()
 
     } else if ( key.ctrl ) {
 
       if(key.name == 'left' ) {
         //go to start of previous word
         if(doc.isFirst())
-          doc.up().end().move()
+          doc.up().end()
         else
-          doc.prev().move()  
-        pref()
+          doc.prev()
+        doc.move().pref()
       }
       if(key.name == 'right') {
         //go to end of next word
         if(doc.isLast())
-          doc.down().start().move()
+          doc.down().start()
         else
-          doc.next().move()  
-        pref()
+          doc.next()
+        doc.move().pref()
       }
+
+      //should the next two use toPref() ?
 
       //start of the previous non whitespace line.
       if(key.name == 'up')
-        doc.prevSection().start().move()
+        doc.prevSection().start().toPref().move()
       
       //start of the previous non whitespace line.
       if(key.name == 'down') {
         if(doc.isLastLine()) doc.end()
         else doc.nextSection().start()
-        doc.move() 
+        doc.toPref().move()
       }
 
       if(key.name == 'home') doc.firstLine().start().move()
@@ -75,12 +74,12 @@ module.exports = function (doc, keys, cursor) {
     
     if(key.name == 'pageup') {
         doc.row = Math.max(doc.row - rc.page, 0)
-        doc.move()
+        doc.toPref().move()
     }
 
     if(key.name == 'pagedown') {
       doc.row = Math.min(doc.row + rc.page, doc.lines.length - 1)
-      doc.move()
+      doc.toPref().move()
     }
   })
 }
